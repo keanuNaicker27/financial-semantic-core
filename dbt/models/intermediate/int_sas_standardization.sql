3476c12c3ec4a7bf031c75c5c8b49e297835be52
Keanu Naicker <keanu.naicker27@gmail.com>
2026-01-05 16:53:14 +0100

    Update and rename int_rgs_standardization.sql to int_sas_standardization.sql

--- dbt/models/intermediate/int_sas_standardization.sql
+++ dbt/models/intermediate/int_sas_standardization.sql
@@ -1,13 +1,13 @@
 {{ config(
-    description="Maps Business Central G/L Entries to RGS 3.5 standardized codes."
+    description="Maps Business Central G/L Entries to SAS 3.5 standardized codes."
 ) }}
 
 WITH gl_entries AS (
     SELECT * FROM {{ ref('stg_bc_gl_entries') }}
 ),
 
-rgs_mapping AS (
-    SELECT * FROM {{ ref('seed_rgs_mapping_v35') }}
+sas_mapping AS (
+    SELECT * FROM {{ ref('seed_sas_mapping_v35') }}
 )
 
 SELECT
@@ -20,10 +20,10 @@ SELECT
     e.gl_account_no AS internal_account_code,
     e.description AS internal_description,
 
-    -- RGS Mapping (The Standardization)
-    m.rgs_code,
-    m.rgs_label_nl,
-    m.rgs_category, -- e.g., 'Activa', 'Passiva', 'Baten'
+    -- sas Mapping (The Standardization)
+    m.sas_code,
+    m.sas_label_nl,
+    m.sas_category, -- e.g., 'Activa', 'Passiva', 'Baten'
 
     -- Financial Measures
     e.amount,
@@ -31,12 +31,12 @@ SELECT
     e.credit_amount,
     
     -- Audit Metadata
-    {{ dbt_utils.generate_surrogate_key(['e.entry_no', 'm.rgs_code']) }} as audit_hash_id
+    {{ dbt_utils.generate_surrogate_key(['e.entry_no', 'm.sas_code']) }} as audit_hash_id
 
 FROM gl_entries e
-LEFT JOIN rgs_mapping m 
+LEFT JOIN sas_mapping m 
     ON e.gl_account_no = m.internal_account_no
 
 -- Flagging unmapped accounts for the Data Quality team
-WHERE m.rgs_code IS NOT NULL 
+WHERE m.sas_code IS NOT NULL 
    OR e.amount != 0
